e0111e9aa6433b65fafab24712c448b7144a71d8
tasneem shehadeh <shhadahatasneem@gmail.com>
2023-05-06 22:36:05 +0300

    add

--- js/app.js
+++ js/app.js
@@ -1,30 +1,29 @@
 let profiles = [
    {
         username: 'eva',
-        gender : 'female',
+        gender : 'Female',
         age: 24,
-        birthday: '04 February 1990',
+        birthday: '06 May 1990',
     },
     {
         username: 'Mohammad',
         gender : 'Male',
         age: 24,
-        birthday: '04 May 1990',
+        birthday: '06 May 2000',
     },
     {
         username: 'Majd',
         gender : 'Female',
         age: 24,
-        birthday: '04 May 1990',
+        birthday: '06 May 1993',
     },
     {
         username: 'Rawan',
         gender : 'Female',
         age: 24,
-        birthday: '05 May 1990',
+        birthday: '06 May 1999',
     }
 ]
-
 let profilesContainer = document.getElementById('birthday__profile-container')
 let currentDate= new Date();
 let  month = currentDate.toLocaleString('default', { month: 'long' });
@@ -32,17 +31,57 @@ let day = currentDate.getDay();
 console.log(day)
 console.log(month);
 console.log(currentDate);
+let count=0;
+
 for(let profile of profiles){
+    let gender = profile.gender;
+  
  let  birthday = profile.birthday.split(' ')
+ const birthYear = new Date(profile.birthday).getFullYear();
+ const currentYear = new Date().getFullYear();
+ const age = currentYear - birthYear;
+ console.log(age);
+
     if(birthday[1] === month && birthday[0] == day ){
+       
+       
+        
+        if(gender =='Female'){  
     //Way 1 : The easy way
+   
         profilesContainer.innerHTML += ` <div class="birthday__profile">
         <img src="assets/img/female.png" alt="">
         <div class="birthday__info">
             <p>${profile.username}</p>
-            <p> ${profile.age}</p>
+            <p> ${age}</p>
+           
         </div>
     </div>`
+        }
+    else{
+        profilesContainer.innerHTML += ` <div class="birthday__profile">
+        <img src="assets/img/male.png" alt="">
+        <div class="birthday__info">
+            <p>${profile.username}</p>
+            <p> ${age}</p>
+        </div>
+    </div>`
+        }
+        
+count++;
+}
+
+}
+
+profilesContainer.innerHTML += ` <id =count>
+<p> ${count } birthdays today</p>
+
+</div>`
+
+    function Clear(){  
+        profilesContainer.remove();  
+      }  
+       
     //Way 2 : The hard way
         //  let profileCard =  document.createElement('div')
         // profileCard.className='birthday__profile';
@@ -60,6 +99,3 @@ for(let profile of profiles){
         //age.appendChild(document.createTextNode(profile.age))
         //birthdayInfo.appendChild(age)
         //console.log(profile)
-    }
-}
-   
